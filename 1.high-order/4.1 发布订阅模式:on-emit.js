bbff0512e7c7c94bef230e6e35fd606b9f198ebd
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-04 15:55:39 +0800

    2.4 on-emit发布订阅模式

---
+++ 1.high-order/4.1 发布订阅模式:on-emit.js
@@ -0,0 +1,25 @@
+let fs = require("fs");
+let school = {};
+let event = {
+  arr: [],
+  emit() {
+    this.arr.forEach((fn) => fn());
+  },
+  on(fn) {
+    this.arr.push(fn);
+  },
+};
+event.on(function() {
+  if(Object.keys(school).length === 2) {
+    console.log(school);
+  }
+})
+fs.readFile("./name.txt", "utf8", function (err, data) {
+  school.name = data;
+  event.emit();
+});
+fs.readFile("./age.txt", "utf8", function (err, data) {
+  school.age = data;
+  event.emit();
+});
+
