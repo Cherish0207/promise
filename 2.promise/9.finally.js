800def1e2ae1a54446ce9616689443865f984060
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-05 22:01:41 +0800

    3.10.2 finally用法-返回promise

--- 2.promise/9.finally.js
+++ 2.promise/9.finally.js
@@ -1,11 +1,22 @@
 // 1. finally 表示的不是最终的意思，而是无论如何都会执行的意思
-// 2. finally 不会影响 then 和 catch 获取的结果
+// 2. finally 不会影响 then 和 catch 获取的结果 --
+// 3. 如果返回promise，会等待这个 promise 也执行完毕,
+//       如果是成功的 promise，会用之前的成功结果
+//       但如果是失败的 promise，会用他的失败原因传给下一个
+
 Promise.resolve(12)
-  .then((data) => {
-    console.log(data, "success");
-  })
+// Promise.reject(12)
   .finally(() => {
     console.log("final");
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(1); // err 1
+        // resolve(1); // 12 success
+      }, 1000);
+    });
+  })
+  .then((data) => {
+    console.log(data, "success");
   })
   .catch((err) => {
     console.log("err", err);
