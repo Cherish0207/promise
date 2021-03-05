9c708bf9d2f0d262611001b8245307ed155a0894
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-05 13:56:21 +0800

    3.5 promise值的穿透.2.等价于

--- 2.promise/5.值的穿透.js
+++ 2.promise/5.值的穿透.js
@@ -1,7 +1,9 @@
-// const Mypromise = require("./0. mypromise");
-let p = new Promise((resolve, reject) => {
+const Mypromise = require("./0. mypromise");
+let p = new Mypromise((resolve, reject) => {
   resolve(1);
 });
-p.then().then().then((data)=>{
-  console.log(data);
-})
+p.then((data) => data)
+  .then((data) => data)
+  .then((data) => {
+    console.log(data);
+  });
