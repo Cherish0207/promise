12bf8594fbb640d34515f256174ef64e375535c4
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-05 13:35:24 +0800

    3.4.5 promise链式调用-返回promise处理5--返回的promise继续resolve（promise）问题

--- 2.promise/4.promise链式调用.js
+++ 2.promise/4.promise链式调用.js
@@ -5,14 +5,30 @@ let p = new Mypromise((resolve, reject) => {
 let promise2 = p.then((data) => {
   return new Mypromise((resolve, reject) => {
     setTimeout(() => {
-      // resolve("success");
-      reject("error");
+      resolve(
+        new Mypromise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(2000);
+          });
+        })
+      );
     });
   });
 });
 promise2.then(
   (data) => {
-    console.log("success:" + data);
+    console.dir(data);
+    /**
+     Mypromise {
+        states: { PENDING: 'PENDING', REJECTED: 'REJECTED', RESOVED: 'RESOLVED' },
+        status: 'PENDING',
+        value: undefined,
+        reason: undefined,
+        onResolvedCallbacks: [],
+        onRejectedCallbacks: []
+      }
+     */
+    // 原生Promise -->2000
   },
   (err) => {
     console.log("err:" + err);
